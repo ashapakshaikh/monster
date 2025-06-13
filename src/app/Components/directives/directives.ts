44d5b4c306457fd2830fb054ee2da70aa6c662a0
ashapakshaikh02112001@outlook.com <ashapakshaikh02112001@outlook.com>
2025-06-13 20:26:02 +0530

    Project start

---
+++ src/app/Components/directives/directives.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  imports: [CommonModule],
+  templateUrl: './directives.html',
+  styleUrl: './directives.css'
+})
+export class Directives {
+isLogin =true;
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
+import { Navbar } from "./Components/navbar/navbar";
+import { Footer } from "./Components/footer/footer";
 import { RouterOutlet } from '@angular/router';
+import { Directives } from './Components/directives/directives';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    Footer,
+    Directives
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
