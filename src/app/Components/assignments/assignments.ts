e61a4424d31a03084aa7c0e7858db457be57ab0c
ashapakshaikh02112001@outlook.com <ashapakshaikh02112001@outlook.com>
2025-06-19 21:58:27 +0530

    angular form

--- src/app/Components/assignments/assignments.ts
+++ src/app/Components/assignments/assignments.ts
@@ -20,6 +20,8 @@ txtproduct_name:string="";
 txtproduct_quantity:number=0;
 txtproduct_price:number=0;
 imgproductImg_url:string="";
+isUpdate:boolean=false;
+isSubmit:boolean=true;
 
 arrObj:Product[]=[
     {
@@ -71,7 +73,62 @@ Submit(){
     }        
 }
 Edit(prdId:number){
+    this.isSubmit = false;
+      this.isUpdate = true;
+  //console.log(prdId);
+  let obj:Product = this.arrObj.find((item) => item.product_id == prdId) as Product;
+  this.txtproduct_id = obj.product_id;
+  this.txtproduct_name = obj.product_name;
+  this.txtproduct_quantity = obj.product_quantity;
+  this.txtproduct_price = obj.product_price;
+  this.imgproductImg_url = obj.productImg_url;
+
+}
+Delete(prdId:number){
   console.log(prdId);
-  this.txtproduct_id=3;
+  let index = this.arrObj.findIndex((item) => item.product_id == prdId);
+  if (index > -1) {
+    this.arrObj.splice(index, 1);
+    alert("Product details deleted sucessfully...!!!");
+  } else {
+    alert("Product not found...!!!");
+  }
+  this.txtproduct_id = 0;
+  this.txtproduct_name = "";
+  this.txtproduct_quantity = 0;
+  this.txtproduct_price = 0;
+  this.imgproductImg_url = "";
+}
+Update(){
+  if (this.txtproduct_id==0) {
+    alert("Please enter the product id...!!!");
+  }else if(this.txtproduct_name==""){
+    alert("Please enter the product name...!!!");
+  }else if(this.txtproduct_quantity==0 || ""){
+    alert("Please enter the product quantity...!!!");
+  }else if(this.txtproduct_price==0 || ""){
+    alert("Please enter the product price...!!!");
+  }else{     
+    let index = this.arrObj.findIndex((item) => item.product_id == this.txtproduct_id);
+    if (index > -1) {
+      //this.isSubmit = false;
+      //this.isUpdate = true;
+      // Update the product details
+      this.arrObj[index] = {
+        product_id: this.txtproduct_id,
+        product_name: this.txtproduct_name,
+        product_price: this.txtproduct_price,
+        product_quantity: this.txtproduct_quantity,
+        productImg_url: this.imgproductImg_url
+      };
+      alert("Product details updated sucessfully...!!!");
+    } else {
+      alert("Product not found...!!!");
+    }
+    this.txtproduct_id=0;
+    this.txtproduct_name="";
+    this.txtproduct_price=0;
+    this.txtproduct_quantity=0;
+  }
 }
 }
